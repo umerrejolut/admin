45baa011796d71dddffde63a2aa90c03fd8b5638
Yash Sisodiya <yash.s@rejolut.com>
2024-03-01 12:37:18 +0530

    fix: husky fixes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: [
+    '@commitlint/config-conventional', // scoped packages are not prefixed
+  ],
+};
